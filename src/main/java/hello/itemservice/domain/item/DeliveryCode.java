e6d0eb673a6b7d9e8afa2cc6176bf459703b4e58
goosebeomsu <captain9697@naver.com>
2022-10-06 01:59:42 +0900

    체크, 라디오, 셀렉트 학습

--- src/main/java/hello/itemservice/domain/item/DeliveryCode.java
+++ src/main/java/hello/itemservice/domain/item/DeliveryCode.java
@@ -3,10 +3,11 @@ package hello.itemservice.domain.item;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.Arrays;
+
 @Data
 @AllArgsConstructor
 public class DeliveryCode {
-
     private String code;
     private String displayName;
 }
